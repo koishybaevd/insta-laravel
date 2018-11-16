1fc34196a0b3e9f6b93d0f40309581815b6eaa25
Dastan <koishybaevdb@gmail.com>
2018-11-16 23:01:49 +0500

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    $img = Image::make('https://images.pexels.com/photos/34950/pexels-photo.jpg')->resize(100, 100);
-
-    return $img->response('jpg');
+    return view('welcome');
 });
 
 Auth::routes();
 
+Route::get('/avatar', function() {
+    return view('auth/avatar');
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/users/{user}', 'UserController@show');
+
+Route::resource('posts', 'PostController');
+Route::get('/posts/getImage/{img_src}', 'PostController@getImage')->name('getImage');
