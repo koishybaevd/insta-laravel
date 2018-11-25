b70bd2be0407769120477fbc832414679ce36d28
Dastan <koishybaevdb@gmail.com>
2018-11-25 16:40:27 +0500

    Change routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Auth::routes();
 
-Route::get('/avatar', function() {
-    return view('auth/avatar');
-});
+Route::get('/setprofileimage', function() {
+    return view('auth/setprofileimage');
+})->name('setProfileImage');
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/seacrh', 'SearchController@index')->name('search');
 
-Route::get('/users/{user}', 'UserController@show');
+Route::get('/users', 'UserController@index')->name('users');
+Route::get('/users/{user}', 'UserController@show')->name('profile');
+Route::get('/users/{user}/edit', 'UserController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UserController@update')->name('users.update')->middleware('auth');
 
 Route::resource('posts', 'PostController');
-Route::get('/posts/getImage/{img_src}', 'PostController@getImage')->name('getImage');
+
+Route::get('/getImage/{img_src}', 'ImageController@getImage')->name('getImage');
